76fc76c2f324b2889f17cfcf1c172041ca24e5e6
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-12-20 08:23:23 -0700

    Add feature test for viewing all hot reads

--- spec/features/unathenticated_user_views_top_10_reads_spec.rb
+++ spec/features/unathenticated_user_views_top_10_reads_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.feature "Unathenticated User", type: :feature do
   it "can view top 10 hot reads" do
-    12.times { |x| Read.create(title: "#{x}", url: "http://#{x}.com", read_at: "time") }
+    12.times do |x|
+      Read.create( title: "#{x}",
+                   url: "http://#{x}.com",
+                   read_at: "#{x} hours" )
+    end
 
     visit root_path
 
+    expect(Read.count).to eq(12)
+    within('.latest-reads') do
+      expect(page).to have_selector('.hot-read', count: 10)
+    end
+  end
 
+  it "can see top reads from last 24 hours" do
+    
   end
 end
