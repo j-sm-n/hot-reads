27597f3ad3c0f9f83864528d9d44366d3a29e041
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-10 22:43:48 -0700

    Add find or initialize by method

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -3,20 +3,19 @@ class ReadsController < ApplicationController
 
   def index
     @hot_reads = Read.hot
+
+    respond_to do |format|
+      format.json {render json: @hot_links}
+      format.html
+    end
   end
 
   def create
-    @read = Read.new(read_params)
+    @read = Read.find_or_initialize_by(url: params[:url])
     if @read.save
       render json: @read, status: 201
     else
       render json: @read.errors.full_messages, status: 500
     end
-  end
-
-  private
-
-  def read_params
-    params.permit(:title, :url)
   end
 end
