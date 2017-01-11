2a1629bd75bfc4718fc4b3937d8bcbeb46cf47b8
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-10 22:40:45 -0700

    Add new migration for reads

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,7 +1,2 @@
 class Read < ApplicationRecord
-  scope :hot, -> {
-    select('reads').where('created_at > ?', Time.now - 1.day).limit(10).group(:title).count
-    # select('links').where('links.created_at > ?', Time.now - 1.day)
-    #   .order('count("links".id) DESC').limit(10)
-  }
 end
--- db/migrate/20170111054030_create_reads.rb
+++ db/migrate/20170111054030_create_reads.rb
@@ -1,10 +1,9 @@
 class CreateReads < ActiveRecord::Migration[5.0]
   def change
     create_table :reads do |t|
-      t.string :title
       t.string :url
-      t.string :read_at
-      
+      t.integer :count
+
       t.timestamps
     end
   end
